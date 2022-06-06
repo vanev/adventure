61972c0112eea437de4cca5ded363e5f2008db5d
Evan Siegel <siegel.evan@gmail.com>
2022-06-06 08:39:31 -0400

    Introduces Colors

---
+++ src/Color.ts
@@ -0,0 +1,28 @@
+enum Color {
+  // https://coolors.co/palette/f8f9fa-e9ecef-dee2e6-ced4da-adb5bd-6c757d-495057-343a40-212529
+  DarkBlack = "#212529",
+  MidBlack = "#343a40",
+  LightBlack = "#495057",
+  DarkGrey = "#6c757d",
+  MidGrey = "#adb5bd",
+  LightGrey = "#ced4da",
+  DarkWhite = "#dee2e6",
+  MidWhite = "#e9ecef",
+  LightWhite = "#f8f9fa",
+
+  // https://coolors.co/palette/cad2c5-84a98c-52796f-354f52-2f3e46
+  DeepGreen = "#2f3e46",
+  DarkGreen = "#354f52",
+  MidGreen = "#52796f",
+  LightGreen = "#84a98c",
+  BrightGreen = "#cad2c5",
+
+  // https://coolors.co/palette/355070-6d597a-b56576-e56b6f-eaac8b
+  DeepPurple = "#355070",
+  DarkPurple = "#6d597a",
+  MidPurple = "#b56576",
+  LightPurple = "#e56b6f",
+  BrightPurple = "#eaac8b",
+}
+
+export default Color;
