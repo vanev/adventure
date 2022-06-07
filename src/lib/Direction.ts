f2d1267f822342c80286178be1e0b250c64dfff9
Evan Siegel <siegel.evan@gmail.com>
2022-06-07 07:56:54 -0400

    Introduces Direction

---
+++ src/lib/Direction.ts
@@ -0,0 +1,5 @@
+export type Cardinal = "North" | "South" | "East" | "West";
+
+export type Ordinal = "Northeast" | "Southeast" | "Southwest" | "Northwest";
+
+export type Direction = Cardinal | Ordinal;
