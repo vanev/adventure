ccc5c701fe50f494dce626cee82e106fd7f5e3da
Evan Siegel <siegel.evan@gmail.com>
2023-05-16 10:47:45 -0400

    Moves textPosition Query to System

--- src/queries/textPosition.ts
+++
@@ -1,9 +0,0 @@
-import { query } from "../ECS";
-import TextComponent from "../components/Text";
-import PositionComponent from "../components/Position";
-
-const textPosition = query({
-  all: new Set([TextComponent, PositionComponent]),
-});
-
-export default textPosition;
--- src/systems/textRender.ts
+++ src/systems/textRender.ts
@@ -1,15 +1,18 @@
-import { system } from "../ECS";
-import PositionComponent from "../components/Position";
-import TextComponent from "../components/Text";
-import textPositionQuery from "../queries/textPosition";
+import { system, query } from "../ECS";
+import Position from "../components/Position";
+import Text from "../components/Text";
 import Display from "../UI/Display";
 import Tick from "../Tick";
+
+const textPositionQuery = query({
+  all: new Set([Text, Position]),
+});
 
 const textRender = (display: Display) =>
   system([textPositionQuery], ([textPositionResults]) => (tick: Tick) => {
     for (const [id, entity] of textPositionResults()) {
-      const position = entity.getComponent(PositionComponent);
-      const text = entity.getComponent(TextComponent);
+      const position = entity.getComponent(Position);
+      const text = entity.getComponent(Text);
       display.drawText(position.data, text.data);
     }
   });
